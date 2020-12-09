14c1f73cd2a315445fc64f9100f053e83193e1d6
SeongBong <chopisal@gmail.com>
2020-12-09 22:41:27 +0900

    accordionMenu

--- config/navigation.js
+++ config/navigation.js
@@ -3,11 +3,13 @@ import TravelList from "../Screens/TravelList";
 import PhotographyList from "../Screens/PhotographyList";
 import CardsList from "../Screens/CardsList";
 import CarsList from "../Screens/CarsList";
+import AccordionMenu from "../Screens/AccordionMenu";
 
 export default [
   { name: "SalonList", label: "Salon", component: SalonList },
   { name: "TravelList", label: "Travel", component: TravelList },
   { name: "PhotographyList", label: "Photography", component: PhotographyList },
   { name: "CardsList", label: "CardsList", component: CardsList },
-  { name: "CarsList", label: "Cars", component: CarsList }
+  { name: "CarsList", label: "Cars", component: CarsList },
+  { name: "AccordionMenu", label: "AccordionMenu", component: AccordionMenu },
 ];
